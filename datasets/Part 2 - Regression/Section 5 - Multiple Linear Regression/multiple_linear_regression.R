d2949302bccc3882cb4ab244604114bdb552c8a0
handielg <66563491+handielg@users.noreply.github.com>
2020-06-11 17:50:40 -0500

    Update multiple_linear_regression.R

--- datasets/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
+++ datasets/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
@@ -48,3 +48,24 @@ summary(regression)
 regression = lm(formula = Profit ~ R.D.Spend,
                 data = dataset)
 summary(regression)
+
+
+# Si quieremos tener una implementación automática de la eliminación hacia atrás en R:
+
+backwardElimination <- function(x, sl) {
+  numVars = length(x)
+  for (i in c(1:numVars)){
+    regressor = lm(formula = Profit ~ ., data = x)
+    maxVar = max(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"])
+    if (maxVar > sl){
+      j = which(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"] == maxVar)
+      x = x[, -j]
+    }
+    numVars = numVars - 1
+  }
+  return(summary(regressor))
+}
+ 
+SL = 0.05
+dataset = dataset[, c(1,2,3,4,5)]
+backwardElimination(training_set, SL)
