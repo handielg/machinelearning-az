4bed6fcf2e4b81648951df172fe362781ad5ca8e
handielg <66563491+handielg@users.noreply.github.com>
2020-06-20 06:39:47 -0500

    Update apriori.R

--- datasets/Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.R
+++ datasets/Part 5 - Association Rule Learning/Section 28 - Apriori/apriori.R
@@ -13,10 +13,10 @@ itemFrequencyPlot(dataset, topN = 10)
 
 # Entrenar algoritmo Apriori con el dataset
 rules = apriori(data = dataset, 
-                parameter = list(support = 0.004, confidence = 0.2))
+                parameter = list(support = 0.004, confidence = 0.2)) #Un nivel de confianza más bajo nos permite arrojar más reglas.
   
 # Visualización de los resultados
-inspect(sort(rules, by = 'lift')[1:10])
+inspect(sort(rules, by = 'lift')[1:10]) #Escogemos las 10 reglas de asociación mas fuertes.
   
 plot(rules, method = "graph", engine = "htmlwidget")
 
