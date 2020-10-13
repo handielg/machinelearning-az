993e6f91f699fb7d19d76fef4debecc98672fed1
handielg <66563491+handielg@users.noreply.github.com>
2020-10-13 10:43:46 +0200

    Update simple_linear_regression.py

--- datasets/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ datasets/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -1,6 +1,3 @@
-[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/googlecolab/colabtools/blob/master/notebooks/colab-github-demo.ipynb)
-
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
